0c6d5f9d437901a54386cda5cfc367ab58d76790
John T. Wodder II <git@varonathe.org>
2017-02-14 18:41:04 +0000

    Ensure the tests run in a UTF-8 locale
    Specifically, the `test_py3_textstr` test; this shouldn't make a difference to
    any other tests.

--- test/test_py3.py
+++ test/test_py3.py
@@ -1,14 +1,13 @@
+from   __future__         import print_function
 import sys
 import pytest
-import six
 from   in_place           import InPlace
 from   test_in_place_util import UNICODE, pylistdir
 
 pytestmark = pytest.mark.skipif(sys.version_info[0] < 3, reason='Python 3 only')
 
-def test_py3_textstr(tmpdir, monkeypatch):
+def test_py3_textstr(tmpdir):
     """ Assert that `InPlace` works with text strings in Python 3 """
-    monkeypatch.setenv('LC_ALL', 'utf-8')
     assert pylistdir(tmpdir) == []
     p = tmpdir.join("file.txt")
     p.write_text(UNICODE, 'utf-8')
@@ -16,6 +15,6 @@ def test_py3_textstr(tmpdir, monkeypatch):
         txt = fp.read()
         assert isinstance(txt, str)
         assert txt == UNICODE
-        six.print_(UNICODE, file=fp)
+        print(UNICODE, file=fp)
     assert pylistdir(tmpdir) == ['file.txt']
     assert p.read_text('utf-8') == UNICODE + '\n'
