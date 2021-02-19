c136ee96237a05cb717c777dd33b9a3dff9b0015
John T. Wodder II <git@varonathe.org>
2021-02-19 22:29:50 +0000

    Handle different default encoding on Windows in tests

--- test/test_py3.py
+++ test/test_py3.py
@@ -1,3 +1,4 @@
+import locale
 import pytest
 from   in_place           import InPlace
 from   test_in_place_util import UNICODE, pylistdir
@@ -6,20 +7,20 @@ def test_py3_textstr(tmpdir):
     """ Assert that `InPlace` works with text strings in Python 3 """
     assert pylistdir(tmpdir) == []
     p = tmpdir.join("file.txt")
-    p.write_text(UNICODE, 'utf-8')
+    p.write_text(UNICODE, locale.getpreferredencoding())
     with InPlace(str(p)) as fp:
         txt = fp.read()
         assert isinstance(txt, str)
         assert txt == UNICODE
         print(UNICODE, file=fp)
     assert pylistdir(tmpdir) == ['file.txt']
-    assert p.read_text('utf-8') == UNICODE + '\n'
+    assert p.read_text(locale.getpreferredencoding()) == UNICODE + '\n'
 
 def test_py3_not_bytestr(tmpdir):
     """ Assert that `InPlace` does not work with byte strings in Python 3 """
     assert pylistdir(tmpdir) == []
     p = tmpdir.join("file.txt")
-    p.write_text(UNICODE, 'utf-8')
+    p.write_text(UNICODE, locale.getpreferredencoding())
     with InPlace(str(p)) as fp:
         txt = fp.read()
         assert isinstance(txt, str)
